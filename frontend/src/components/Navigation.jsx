9e70eb524861e4f1ad5bfff6395a57d049f215e0
juan-gil-05 <gilj06@hotmail.com>
2026-06-03 12:06:47 +0200

    feat: integrate Tailwind CSS and update components for styling
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated App component to include a Toaster for notifications.
    - Enhanced Navigation component with improved styling and layout.
    - Styled TaskCard component for better visual presentation.
    - Modified TaskList component to use a grid layout for task cards.
    - Adjusted index.css to properly import Tailwind CSS.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const Navigation = () => {
+function Navigation() {
     return (
-        <div>
-            <Link to="/tasks"> <h1>Tasks</h1> </Link>
-            <Link to="/tasks-create"> Create tasks </Link>
+        <div className="flex justify-between py-3 items-center">
+            <Link to="/tasks">
+                <h1 className="font-bold text-3xl mb-4">Tasks App</h1>
+            </Link>
+            <button className="bg-indigo-500 p-3 rounded-lg">
+                <Link to="/tasks-create">Create Task</Link>
+            </button>
         </div>
-    )
+    );
 }
 
 export default Navigation
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -15,7 +15,7 @@ const TaskList = () => {
     }, [])
 
 
-    return <div>
+    return <div className='grid grid-cols-3 gap-3'>
         {tasks.map(task => (
             <TaskCard key={task.id} task={task} />
         ))}
